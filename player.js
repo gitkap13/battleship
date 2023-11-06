f87a49ab20f0757d36ff1ba012dd66fc982f3c27
gitkap13 <116759485+gitkap13@users.noreply.github.com>
2023-11-06 13:48:41 -0500

    create player module

--- player.js
+++ player.js
@@ -1,32 +1,30 @@
-const makeBoard = require('./gameboard');
+const makeBoard = require("./gameboard");
+const createHumanPlayer = (name) => {
+  const isCom = false;
+  const board = makeBoard();
+  const targetBoard = [];
+  for (let i = 0; i < 100; i++)
+    [targetBoard.push({ location: i, attacked: false, hit: null })];
+  return { isCom, board, targetBoard };
+};
+const createComPlayer = () => {
+  const isCom = true;
+  const board = makeBoard();
+  const targetBoard = [];
+  for (let i = 0; i < 100; i++)
+    [targetBoard.push({ location: i, attacked: false, hit: null })];
+  const availableAttacks = targetBoard.map((obj) => {
+    return obj.location;
+  });
+  const randomAttack = () => {
+    let target = Math.floor(Math.random() * availableAttacks.length);
+    console.log(target);
+    availableAttacks.splice(target, 1);
+    return target;
+  };
 
-function createPlayer(name='') {
-    const isCPU = false;
-    const board = makeBoard();
-    const opBoard = board.grid; 
-    return {name: name, board: board, opBoard: opBoard, isCPU: isCPU}
-}
+  return { isCom, board, targetBoard, randomAttack, availableAttacks };
+};
 
-function createComPlayer(name='Computer') {
-    const isCPU = true;
-    const board = makeBoard();
-    let possibleAttacks = (function() {
-        let arr = []
-        for (let i = 0; i < 10; i++) {
-          for (let j = 0; j < 10; j++) {
-            arr.push([i, j]);
-          }
-        }
-        return arr
-      })();
-    const attack = () => {
-        let random = Math.floor(Math.random() * possibleAttacks.length)
-        let attackSquare = possibleAttacks[random];
-        possibleAttacks.splice(random, 1);
-        return attackSquare
-    }
-
-    return {name: name, board: board, possibleAttacks: possibleAttacks, attack: attack}
-}
-
-module.exports = {createPlayer, createComPlayer}
+exports.createHumanPlayer = createHumanPlayer;
+exports.createComPlayer = createComPlayer;
--- player.test.js
+++ player.test.js
@@ -1,7 +1,23 @@
-const createPlayer = require('./player');
-const john = createPlayer.createPlayer('john');
-const comp = createPlayer.createComPlayer();
-console.log(comp)
-test('returns player object with board', () => {
-    expect(john).toBeDefined()
+const {createHumanPlayer, createComPlayer} = require('./player')
+
+describe('creates player with gameboards for self and target', () => {
+    let testPlayer = createHumanPlayer('testplayer');
+    let testComPlayer = createComPlayer();
+    test('player is returned', () => {
+        expect(testPlayer.board).toBeDefined();
+        expect(testPlayer.targetBoard).toBeDefined();
+    });
+    test('computer player is returned', () => {
+        expect(testComPlayer.board).toBeDefined();
+        expect(testComPlayer.targetBoard).toBeDefined();
+    })
+    test('computer has list of available attack coordinates', () => {
+        expect(testComPlayer.availableAttacks.length).toBe(100)
+    })
+    test('random attack returns number between 0 - 99', () => {
+        expect(testComPlayer.randomAttack()).toBeGreaterThanOrEqual(0);
+        testComPlayer.randomAttack();
+        expect(testComPlayer.availableAttacks.length).toBeLessThan(99);
+    })
+
 })
