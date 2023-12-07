5b8430d579dfc8f3a3b8d43c9801e94edfbd99d4
gitkap13 <116759485+gitkap13@users.noreply.github.com>
2023-12-07 14:19:46 -0500

    clean code

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
-import './style.css';
-const $ = require('jquery');
-const getShips = require('./components/placeShips')
+import "./style.css";
+const $ = require("jquery");
+const getShips = require("./components/placeShips");
 window.jQuery = $;
 window.$ = $;
-const startGame = async () => {
-    const ships = getShips();
-    };
-
-startGame();
+getShips();
